fac25fafdfd745c61df0249cbb19e27ec192bc7c
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-09-11 20:00:08 +0300

    Rename `deploy` to `publish` and add `tasks`

---
+++ scripts/tasks-extra.bash
@@ -0,0 +1,15 @@
+#!/dev/null
+
+cat <<EOS
+
+modaclouds-services-knowledgebase@package :
+modaclouds-services-monitoring-manager@package :
+modaclouds-services-monitoring-dda@package :
+modaclouds-services-monitoring-sda-matlab@package :
+modaclouds-services-monitoring-sda-weka@package :
+
+modaclouds-platform-core@package : mosaic-node-boot@package
+
+EOS
+
+exit 0
