0caf4132c48a3d0b31e2e24dc1cdd19c2ef751f6
Ciprian Dorin Craciun <ciprian@volution.ro>
2014-10-09 11:53:32 +0300

    Add metric related packages

---
+++ packages/modaclouds-services-metric-explorer/sources/service-run.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e -E -u -o pipefail -o noclobber -o noglob +o braceexpand || exit 1
+trap 'printf "[ee] failed: %s\n" "${BASH_COMMAND}" >&2' ERR || exit 1
+
+exec </dev/null >&2
+
+exit 1
--- scripts/tasks-extra.bash
+++ scripts/tasks-extra.bash
@@ -7,6 +7,8 @@ modaclouds-services-monitoring-manager@package :
 modaclouds-services-monitoring-dda@package :
 modaclouds-services-monitoring-sda-matlab@package :
 modaclouds-services-monitoring-sda-weka@package :
+modaclouds-services-metric-explorer@package :
+modaclouds-services-metric-importer@package :
 
 modaclouds-platform-core@package : mosaic-node-boot@package
 
