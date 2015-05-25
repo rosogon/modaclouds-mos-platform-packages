8dc999549b77b65171635e9fb748aa60bb5b9cc2
Ciprian Dorin Craciun <ciprian@volution.ro>
2015-05-25 15:36:47 +0300

    [template]  Add logback

--- packages/_modaclouds-services-template/application/src/main/java/RunService.java
+++ packages/_modaclouds-services-template/application/src/main/java/RunService.java
@@ -1,10 +1,16 @@
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 public class RunService {
 	
 	public static void main (String[] arguments) throws Throwable {
 		
-		System.err.printf ("[ii] starting example service...\n");
+		Logger logger = LoggerFactory.getLogger (RunService.class);
+		
+		logger.info ("starting example service...");
 		
 		String listenIp = System.getenv ("MODACLOUDS_SERVICE_X_ENDPOINT_IP");
 		String listenPort = System.getenv ("MODACLOUDS_SERVICE_X_ENDPOINT_PORT");
@@ -12,10 +18,10 @@ public class RunService {
 		String service_Y_Ip = System.getenv ("MODACLOUDS_SERVICE_Y_ENDPOINT_IP");
 		String service_Y_Port = System.getenv ("MODACLOUDS_SERVICE_Y_ENDPOINT_PORT");
 		
-		System.err.printf ("[ii] listening on IP `%s`, port `%s`...\n", listenIp, listenPort);
-		System.err.printf ("[ii] connecting to service Y on IP `%s`, port `%s`...\n", service_Y_Ip, service_Y_Port);
+		logger.info ("listening on IP `{}`, port `{}`...", listenIp, listenPort);
+		logger.info ("connecting to service Y on IP `{}`, port `{}`...", service_Y_Ip, service_Y_Port);
 		
-		System.err.printf ("[ii] executing...\n");
+		logger.info ("executing...");
 		
 		Thread.sleep (3600 * 1000);
 	}
