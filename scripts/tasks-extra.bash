e3f3efd380f0cfcd48e66bf2da1a00f5aeb393f4
Ciprian Dorin Craciun <ciprian@volution.ro>
2015-06-12 19:47:20 +0300

    Add FG services

--- scripts/tasks-extra.bash
+++ scripts/tasks-extra.bash
@@ -6,31 +6,33 @@ modaclouds-platform-core@package : mosaic-node-boot@package | mosaic-node-boot@p
 
 modaclouds-platform-core@publish :
 
+modaclouds-services-fg-analyzer@package :
+modaclouds-services-fg-local-db@package :
 modaclouds-services-knowledgebase@package :
-modaclouds-services-monitoring-manager@package :
-modaclouds-services-monitoring-dda@package :
-modaclouds-services-monitoring-sda-matlab@package :
-modaclouds-services-monitoring-sda-weka@package :
 modaclouds-services-load-balancer-controller@package :
 modaclouds-services-load-balancer-reasoner@package :
 modaclouds-services-metric-explorer@package :
 modaclouds-services-metric-importer@package :
 modaclouds-services-models-at-runtime@package :
-modaclouds-services-history-db@package :
+modaclouds-services-monitoring-dda@package :
+modaclouds-services-monitoring-manager@package :
+modaclouds-services-monitoring-sda-matlab@package :
+modaclouds-services-monitoring-sda-weka@package :
 modaclouds-services-sla-core@package :
 modaclouds-tools-mdload@package :
 
+modaclouds-services-fg-analyzer@publish :
+modaclouds-services-fg-local-db@publish :
 modaclouds-services-knowledgebase@publish :
-modaclouds-services-monitoring-manager@publish :
-modaclouds-services-monitoring-dda@publish :
-modaclouds-services-monitoring-sda-matlab@publish :
-modaclouds-services-monitoring-sda-weka@publish :
 modaclouds-services-load-balancer-controller@publish :
 modaclouds-services-load-balancer-reasoner@publish :
 modaclouds-services-metric-explorer@publish :
 modaclouds-services-metric-importer@publish :
 modaclouds-services-models-at-runtime@publish :
-modaclouds-services-history-db@publish :
+modaclouds-services-monitoring-dda@publish :
+modaclouds-services-monitoring-manager@publish :
+modaclouds-services-monitoring-sda-matlab@publish :
+modaclouds-services-monitoring-sda-weka@publish :
 modaclouds-services-sla-core@publish :
 modaclouds-tools-mdload@publish :
 
